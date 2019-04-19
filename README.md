f86ce27c58efea60b99846a58a3dabf17299a20b
Tobias Klein <contact@tklein.info>
2019-04-20 00:05:23 +0200

    Updated name in README and license headers to node-sword-interface

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ezra-sword-interface
-ezra-sword-interface is a Javascript nodejs module (using [N-API](https://nodejs.github.io/node-addon-api/)) that wraps the [SWORD library](http://www.crosswire.org/sword/) for use within the [Ezra&nbsp;Project](https://github.com/tobias-klein/ezra-project) bible study software.
+# node-sword-interface
+node-sword-interface is a Javascript nodejs module (using [N-API](https://nodejs.github.io/node-addon-api/)) that wraps the [SWORD library](http://www.crosswire.org/sword/) for use within nodejs applications.
--- node_sword_interface.cpp
+++ node_sword_interface.cpp
@@ -1,19 +1,19 @@
-/* This file is part of ezra-sword-interface.
+/* This file is part of node-sword-interface.
 
    Copyright (C) 2019 Tobias Klein <contact@ezra-project.net>
 
-   ezra-sword-interface is free software: you can redistribute it and/or modify
+   node-sword-interface is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   ezra-sword-interface is distributed in the hope that it will be useful,
+   node-sword-interface is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of 
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
    See the GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with ezra-sword-interface. See the file COPYING.
+   along with node-sword-interface. See the file COPYING.
    If not, see <http://www.gnu.org/licenses/>. */
 
 #include <time.h>
