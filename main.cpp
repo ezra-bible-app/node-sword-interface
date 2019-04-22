8c7835d0e588ea457d7e40cd660553c349ac6b13
Tobias Klein <contact@tklein.info>
2019-04-22 20:10:01 +0200

    Added test code for testing getBookText function

--- main.cpp
+++ main.cpp
@@ -9,43 +9,50 @@ using namespace sword;
 
 int main(int argc, char** argv)
 {
-  SwordFacade sword_facade;
+    SwordFacade sword_facade;
 
-  sword_facade.refreshRemoteSources(true);
+    //sword_facade.refreshRemoteSources(true);
 
-  cout << "REPOSITORIES:" << endl;
-  vector<string> repoNames = sword_facade.getRepoNames();
-  for (unsigned int i = 0; i < repoNames.size(); i++) {
-    cout << repoNames[i] << endl;
-  }
-  cout << endl;
-
-  cout << "English MODULES of CrossWire:" << endl;
-  vector<SWModule*> modules = sword_facade.getRepoModulesByLang("CrossWire", "en");
-  for (unsigned int i = 0; i < modules.size(); i++) {
-    SWModule* currentModule = modules[i];
-
-    cout << currentModule->getName();
-
-    if (currentModule->getConfigEntry("Abbreviation")) {
-      string moduleShortcut = currentModule->getConfigEntry("Abbreviation");
-      cout << " " << moduleShortcut;
+    cout << "REPOSITORIES:" << endl;
+    vector<string> repoNames = sword_facade.getRepoNames();
+    for (unsigned int i = 0; i < repoNames.size(); i++) {
+        cout << repoNames[i] << endl;
     }
-
-    if (currentModule->getConfigEntry("InstallSize")) {
-      string installSize = currentModule->getConfigEntry("InstallSize");
-      cout << " " << installSize;
-    }
-
-    string moduleVersion = currentModule->getConfigEntry("Version");
-    cout << " " << moduleVersion << " ";
-
-    string moduleLocked = currentModule->getConfigEntry("CipherKey") ? "LOCKED" : "";
-    cout << moduleLocked;
-
     cout << endl;
-  }
-  cout << endl;
 
-  return 0;
+    cout << "English MODULES of CrossWire:" << endl;
+    vector<SWModule*> modules = sword_facade.getRepoModulesByLang("CrossWire", "en");
+    for (unsigned int i = 0; i < modules.size(); i++) {
+        SWModule* currentModule = modules[i];
+
+        cout << currentModule->getName();
+
+        if (currentModule->getConfigEntry("Abbreviation")) {
+            string moduleShortcut = currentModule->getConfigEntry("Abbreviation");
+            cout << " " << moduleShortcut;
+        }
+
+        if (currentModule->getConfigEntry("InstallSize")) {
+            string installSize = currentModule->getConfigEntry("InstallSize");
+            cout << " " << installSize;
+        }
+
+        string moduleVersion = currentModule->getConfigEntry("Version");
+        cout << " " << moduleVersion << " ";
+
+        string moduleLocked = currentModule->getConfigEntry("CipherKey") ? "LOCKED" : "";
+        cout << moduleLocked;
+
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Text of 1 John:" << endl;
+    vector<string> verses = sword_facade.getBookText("GerSch", "1John");
+    cout << "Got " << verses.size() << " verses!" << endl;
+    for (int i = 0; i < verses.size(); i++) {
+        cout << verses[i] << endl;
+    }
+
+    return 0;
 }
