1ec1a2bf7507c75d8b6e57856d4e49d16c3ee47e
Tobias Klein <contact@tklein.info>
2019-06-10 10:16:01 +0200

    Adapted rebuild command

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-EZRA_PROJECT_DIR="ezra-project"
-NODE_SWORD_INTERFACE_DIR="node-sword-interface"
+EZRA_PROJECT_DIR="ezra-project-git"
+NODE_SWORD_INTERFACE_DIR="node-sword-interface-git"
 
 cd ..
 rm -rf ${EZRA_PROJECT_DIR}/node_modules/node-sword-interface
 cp -a ${NODE_SWORD_INTERFACE_DIR} ${EZRA_PROJECT_DIR}/node_modules/node-sword-interface
 cd ${EZRA_PROJECT_DIR}
-npm run rebuild-linux
+npm run rebuild
