5760e5b72947fae1a364dc05737c50b9736ca51d
Tobias Klein <contact@tklein.info>
2019-06-15 17:51:44 +0200

    Added a missing 'cd' command

--- build_sword.sh
+++ build_sword.sh
@@ -3,6 +3,7 @@
 svn co http://crosswire.org/svn/sword/tags/sword-1-8-1/ sword
 cd sword
 patch -p0 ../sword_icu.patch
+cd ..
 mkdir -p sword_build
 cd sword_build
 cmake -DLIBSWORD_LIBRARY_TYPE=Static ../sword
