4d8ddfca8ad3745cb173cd3fe622fbc37eedf024
Tobias Klein <contact@tklein.info>
2019-06-15 17:48:13 +0200

    Added a patch to fix sword compile issues with newer ICU library versions

--- build_sword.sh
+++ build_sword.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 svn co http://crosswire.org/svn/sword/tags/sword-1-8-1/ sword
+cd sword
+patch -p0 ../sword_icu.patch
 mkdir -p sword_build
 cd sword_build
 cmake -DLIBSWORD_LIBRARY_TYPE=Static ../sword
