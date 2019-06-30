4aac6ea83931dc31ff0b148af43b0a8b2d7b5635
Tobias Klein <contact@tklein.info>
2019-06-30 18:51:12 +0200

    Patch CMake configuration, so that GLOBCONFPATH is not overwritten

--- build_sword.sh
+++ build_sword.sh
@@ -2,6 +2,7 @@
 
 svn co http://crosswire.org/svn/sword/tags/sword-1-8-1/ sword
 patch --batch -d sword -p 0 < sword_icu.patch
+patch --batch -d sword -p 0 < sword_globconf.patch
 mkdir -p sword_build
 cd sword_build
 cmake -DLIBSWORD_LIBRARY_TYPE=Static ../sword
