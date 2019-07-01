46978208a72a2713dfa98c4e8b59a7edf213c70d
Tobias Klein <contact@tklein.info>
2019-07-01 07:43:07 +0200

    Only apply Sword ICU patch on Linux

--- build_sword.sh
+++ build_sword.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# CHECKOUT
 svn co http://crosswire.org/svn/sword/tags/sword-1-8-1/ sword
-patch --batch -d sword -p 0 < sword_icu.patch
+
+# PATCHES
+ifeq ($(uname_S),Linux)
+    patch --batch -d sword -p 0 < sword_icu.patch
+endif
+
 patch --batch -d sword -p 0 < sword_globconf.patch
+
+# BUILD
 mkdir -p sword_build
 cd sword_build
 cmake -DLIBSWORD_LIBRARY_TYPE=Static ../sword
