bcedaa7d37b6866d91ae8d42dec24ff7b51ecf2c
Tobias Klein <contact@tklein.info>
2019-12-17 11:33:06 +0100

    Correct indentation of StrongsEntry

--- src/strongs_entry.cpp
+++ src/strongs_entry.cpp
@@ -30,36 +30,36 @@ using namespace sword;
 
 StrongsReference::StrongsReference(string text)
 {
-  this->text = text;
-  this->key = this->parseKey(text);
+    this->text = text;
+    this->key = this->parseKey(text);
 }
 
 string StrongsReference::parseKey(string text)
 {
-  string key = "";
+    string key = "";
 
-  // A reference looks like this:
-  //see GREEK for 2767
-  //
-  // To parse the actual key we first split the string by space, getting the four literals as a result.
-  vector<string> splittedReference = StringHelper::split(text, " ");
+    // A reference looks like this:
+    //see GREEK for 2767
+    //
+    // To parse the actual key we first split the string by space, getting the four literals as a result.
+    vector<string> splittedReference = StringHelper::split(text, " ");
 
-  if (splittedReference.size() == 4) {
-    // The reference language is the first character of the second literal
-    char referenceLang = splittedReference[1][0];
+    if (splittedReference.size() == 4) {
+        // The reference language is the first character of the second literal
+        char referenceLang = splittedReference[1][0];
 
-    // The key is the last / 4th literal
-    // We convert it to a number to get rid of unnecessary leading 0s
-    unsigned int numericKey = stoi(splittedReference[3]);
-    // Then we convert it back into a string
-    string stringKey = to_string(numericKey);
+        // The key is the last / 4th literal
+        // We convert it to a number to get rid of unnecessary leading 0s
+        unsigned int numericKey = stoi(splittedReference[3]);
+        // Then we convert it back into a string
+        string stringKey = to_string(numericKey);
 
-    key = referenceLang + stringKey;
-  } else {
-    key = "";
-  }
+        key = referenceLang + stringKey;
+    } else {
+        key = "";
+    }
 
-  return key;
+    return key;
 }
 
 /*
@@ -67,13 +67,13 @@ string StrongsReference::parseKey(string text)
  */
 bool StrongsReference::isKeyValid()
 {
-  return this->key != "";
+    return this->key != "";
 }
 
 StrongsEntry::StrongsEntry(string key, string rawEntry)
 {
-  this->key = key;
-  this->parseFromRawEntry(rawEntry);
+    this->key = key;
+    this->parseFromRawEntry(rawEntry);
 }
 
 StrongsEntry* StrongsEntry::getStrongsEntry(SWModule* module, string key)
@@ -114,14 +114,14 @@ void StrongsEntry::eraseEmptyLines(vector<string>& lines)
     int maxIterations = 50;
 
     for (int i = 0; i < maxIterations && currentLine == "" && lines.size() > 0; i++) {
-      currentLine = lines[0];
-      StringHelper::trim(currentLine, " ");
-      if (currentLine == "") {
-        lines.erase(lines.begin(), lines.begin() + 1);
-      } else {
-        // Break when reaching the first line with actual content
-        break;
-      }
+        currentLine = lines[0];
+        StringHelper::trim(currentLine, " ");
+        if (currentLine == "") {
+            lines.erase(lines.begin(), lines.begin() + 1);
+        } else {
+            // Break when reaching the first line with actual content
+            break;
+        }
     }
 }
 
@@ -133,23 +133,23 @@ void StrongsEntry::parseDefinitionAndReferences(vector<string>& lines)
     static regex doubleLineBreak = regex("\n\n");
 
     for (unsigned int i = 0; i < lines.size(); i++) {
-      string currentLine = lines[i];
-      if (currentLine.substr(0,5) == " see ") {
-        StringHelper::trim(currentLine);
-        StrongsReference reference(currentLine);
-        // Only put the current line into the list of references if it's not already in there
-        // We do this, because there are Strong's like H3069 (Yhovih) that contain duplicates
-        if (find(rawReferences.begin(), rawReferences.end(), currentLine) == rawReferences.end()) {
-          rawReferences.push_back(currentLine);
+        string currentLine = lines[i];
+        if (currentLine.substr(0,5) == " see ") {
+            StringHelper::trim(currentLine);
+            StrongsReference reference(currentLine);
+            // Only put the current line into the list of references if it's not already in there
+            // We do this, because there are Strong's like H3069 (Yhovih) that contain duplicates
+            if (find(rawReferences.begin(), rawReferences.end(), currentLine) == rawReferences.end()) {
+                rawReferences.push_back(currentLine);
 
-          // We only take references with a valid (non-empty) key
-          if (reference.isKeyValid()) {
-            references.push_back(reference);
-          }
+                // We only take references with a valid (non-empty) key
+                if (reference.isKeyValid()) {
+                    references.push_back(reference);
+                }
+            }
+        } else {
+            definition << currentLine << "\n";
         }
-      } else {
-        definition << currentLine << "\n";
-      }
     }
 
     string definitionString = definition.str();
@@ -173,12 +173,12 @@ void StrongsEntry::parseFromRawEntry(string rawEntry)
     // An example for this is G2147
     // This is necessary so that the parsing works in this specific case
     if (firstCommaPosition < firstLineBreakPosition) {
-      this->rawEntry.replace(firstCommaPosition, 1, "\n");
+        this->rawEntry.replace(firstCommaPosition, 1, "\n");
     }
 
     vector<string> allLines = StringHelper::split(this->rawEntry, "\n");
     if (allLines.size() == 0) {
-      return;
+        return;
     }
 
     this->parseFirstLine(allLines[0]);
