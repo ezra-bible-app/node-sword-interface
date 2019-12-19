37663f1b77e873acf005fe06987c7bf152c133b5
tobias-klein <contact@tklein.info>
2019-12-19 18:35:41 +0100

    Add patch for ICU64 on macOS

--- build_sword.sh
+++ build_sword.sh
@@ -10,6 +10,11 @@ case "$(uname -s)" in
     # We only apply the Sword ICU patch on Linux
         patch --batch --forward -d sword -p 0 < sword_icu.patch
     ;;
+    Darwin)
+    # We only apply the following patch on macOS
+        export CMAKE_PREFIX_PATH=/usr/local/opt/icu4c
+        patch --batch --forward -d sword -p 0 < sword_mac.patch
+    ;;
 esac
 
 patch --batch --forward -d sword -p 0 < sword_globconf.patch
@@ -17,5 +22,5 @@ patch --batch --forward -d sword -p 0 < sword_globconf.patch
 # BUILD
 mkdir -p sword_build
 cd sword_build
-cmake -DLIBSWORD_LIBRARY_TYPE=Static ../sword
+cmake -DLIBSWORD_LIBRARY_TYPE=Static -DCMAKE_CXX_STANDARD=11 ../sword
 make -j2 sword_static
