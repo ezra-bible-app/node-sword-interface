a9b1b2ef5f63ee7a8795047774a5d4711b3c3a4b
Tobias Klein <contact@tklein.info>
2019-12-30 11:44:31 +0100

    Re-enable special export option for macOS (was accidentally commented)

--- scripts/build_sword.sh
+++ scripts/build_sword.sh
@@ -5,17 +5,17 @@ git clone https://github.com/bibletime/crosswire-sword-mirror sword
 #git -C sword checkout tags/sword-1-8-1
 
 # PATCHES
-#case "$(uname -s)" in
+case "$(uname -s)" in
 #    Linux)
 #    # We only apply the Sword ICU patch on Linux
 #        patch --batch --forward -d sword -p 0 < patch/sword_icu.patch
 #    ;;
-#    Darwin)
-#    # We only apply the following patch on macOS
-#        export CMAKE_PREFIX_PATH=/usr/local/opt/icu4c
+    Darwin)
+    # We only apply the following patch on macOS
+        export CMAKE_PREFIX_PATH=/usr/local/opt/icu4c
 #        patch --batch --forward -d sword -p 0 < patch/sword_mac.patch
-#    ;;
-#esac
+    ;;
+esac
 
 patch --batch --forward -d sword -p 0 < patch/sword_globconf.patch
 
