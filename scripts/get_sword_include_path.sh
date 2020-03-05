971d8169d82d010280ec18da6f74b4eff0391191
Tobias Klein <contact@tklein.info>
2020-03-05 07:35:00 +0100

    Add documentation for new script get_sword_include_path.sh

--- scripts/get_sword_include_path.sh
+++ scripts/get_sword_include_path.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# This script is used by binding.gyp to dynamically determine the include path of the SWORD library.
+# It returns the system include path of the SWORD library if the environment variable
+# LINK_SYSTEM_SWORD has the value 1 or otherwise the path to the locally cloned sword/include directory.
 
 if [ "$LINK_SYSTEM_SWORD" = "1" ]; then
     echo "/usr/include/sword"
