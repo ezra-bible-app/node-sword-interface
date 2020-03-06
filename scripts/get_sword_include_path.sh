206d12a587d9b002bcc8b50431871c07050ecef4
Tobias Klein <contact@tklein.info>
2020-03-06 07:56:51 +0100

    Use pkg-config to get SWORD include path

--- scripts/get_sword_include_path.sh
+++ scripts/get_sword_include_path.sh
@@ -4,7 +4,8 @@
 # LINK_SYSTEM_SWORD has the value 1 or otherwise the path to the locally cloned sword/include directory.
 
 if [ "$LINK_SYSTEM_SWORD" = "1" ]; then
-    echo "/usr/include/sword"
+    # This command turns something like -I/usr/include/sword into /usr/include/sword (it cuts out all character from the 3rd one)
+    pkg-config --cflags sword | cut -c3-
 else
     echo "sword/include"
 fi
