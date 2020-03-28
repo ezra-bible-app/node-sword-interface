22fb9bc7bc7197b29c1e5391a4940ee7f8d8c77b
Tobias Klein <contact@tklein.info>
2020-03-28 09:57:37 +0100

    Update license headers (2019 - 2020)

--- src/file_system_helper.cpp
+++ src/file_system_helper.cpp
@@ -1,6 +1,6 @@
 /* This file is part of node-sword-interface.
 
-   Copyright (C) 2019 Tobias Klein <contact@ezra-project.net>
+   Copyright (C) 2019 - 2020 Tobias Klein <contact@ezra-project.net>
 
    node-sword-interface is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
