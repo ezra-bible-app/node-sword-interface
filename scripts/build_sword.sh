965563e578a41e23f916ee983ac91ac84ab7a249
Tobias Klein <contact@tklein.info>
2020-08-16 09:56:48 +0200

    Use sword version b2baf2 (latest upstream / SVN Rev. 3779)

--- scripts/build_sword.sh
+++ scripts/build_sword.sh
@@ -12,7 +12,7 @@ fi
 
 # CHECKOUT
 git clone https://github.com/bibletime/crosswire-sword-mirror sword
-git -C sword checkout 8edcfc
+git -C sword checkout b2baf2
 
 # PATCHES
 case "$(uname -s)" in
