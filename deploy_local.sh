928fa293a4cca4f3cfb17c653d2b26828ad1c0bd
Tobias Klein <contact@tklein.info>
2021-04-30 20:53:45 +0200

    deploy_local.sh: Use rebuild-nsi command, so that only node-sword-interface is rebuilt

--- deploy_local.sh
+++ deploy_local.sh
@@ -4,4 +4,4 @@ PROJECT_DIR='ezra-project-git'
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf $WORK_DIR/../$PROJECT_DIR/node_modules/node-sword-interface
 cp -a $WORK_DIR $WORK_DIR/../$PROJECT_DIR/node_modules/node-sword-interface
-RESULT="$( cd $WORK_DIR/../${PROJECT_DIR}/ && npm run rebuild )"
+RESULT="$( cd $WORK_DIR/../${PROJECT_DIR}/ && npm run rebuild-nsi )"
