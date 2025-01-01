d2e89b0c1d2c6b0b19d88fd5d8a4b0f6de87535e
Tobias Klein <contact@tklein.info>
2025-01-01 08:26:47 +0100

    Update Copyright header to 2025

--- scripts/build_sword.sh
+++ scripts/build_sword.sh
@@ -2,7 +2,7 @@
 
 #   This file is part of node-sword-interface.
 #
-#   Copyright (C) 2019 - 2024 Tobias Klein <contact@tklein.info>
+#   Copyright (C) 2019 - 2025 Tobias Klein <contact@tklein.info>
 #
 #   node-sword-interface is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
