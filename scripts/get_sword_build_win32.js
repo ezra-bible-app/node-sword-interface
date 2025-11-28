6a3de5678a3f09922ac296553318763e50f958d8
Tobias Klein <contact@tklein.info>
2025-11-28 21:02:24 +0100

    Update license header in get_sword_build_win32.js for clarity and completeness

--- scripts/get_sword_build_win32.js
+++ scripts/get_sword_build_win32.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
-// This file is part of node-sword-interface.
-// This script downloads a release asset (zip) from GitHub, extracts it and
-// renames the directory to sword-build-win32 inside the current working directory.
-//
-// License: GPL-2.0+
+/* This file is part of node-sword-interface.
+
+   Copyright (C) 2019 - 2025 Tobias Klein <contact@tklein.info>
+
+   node-sword-interface is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   node-sword-interface is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   See the GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with node-sword-interface. See the file COPYING.
+   If not, see <http://www.gnu.org/licenses/>. */
 
 const https = require('https');
 const fs = require('fs');
