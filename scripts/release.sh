46e8995022f255066d1199ef2d38a65d8e5e80b7
Tobias Klein <contact@tklein.info>
2025-12-31 10:43:04 +0100

    Refactor release script to set version tag prefix directly in npm version command

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Do not use any prefix for the version tag
-npm config set tag-version-prefix=""
-
 # Increment the patch version (creates commit and tag)
-npm version patch
+npm version patch --tag-version-prefix=""
 
 # Push the changes and the tag
 git push --follow-tags
