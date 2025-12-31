13f5f4671baebcdb3327c7b77f3eef75ef4f9393
Tobias Klein <contact@tklein.info>
2025-12-31 10:41:37 +0100

    Add version tag prefix configuration to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Do not use any prefix for the version tag
+npm config set tag-version-prefix=""
+
 # Increment the patch version (creates commit and tag)
 npm version patch
 
