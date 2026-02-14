5c46f6a41c7a1223091bbe8750835f7bc9132939
Tobias Klein <contact@tklein.info>
2026-02-14 18:01:33 +0100

    Adjust repo name in deploy_local.sh script

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_DIR='ezra-project-git'
+PROJECT_DIR='ezra-bible-app'
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf $WORK_DIR/../$PROJECT_DIR/node_modules/node-sword-interface
 cp -a $WORK_DIR $WORK_DIR/../$PROJECT_DIR/node_modules/node-sword-interface
